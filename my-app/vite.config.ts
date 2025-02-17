28968877605bf00e518417cd51db307548f32898
Steve Krause <sgkrause@gmail.com>
2025-02-17 20:05:34 +0000

    initial commit

---
+++ my-app/vite.config.ts
@@ -0,0 +1,15 @@
+/// <reference types='vite/client' />
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/my-app',
+  server: {
+    host: true,
+    port: 8081,
+    allowedHosts: ["d2otfhqz8weojq.cloudfront.net"]
+  },
+})
