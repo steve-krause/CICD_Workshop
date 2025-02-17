c0706b5acfbbe0adc2d56781182595ecdfa603d0
Steve Krause <sgkrause@gmail.com>
2025-02-17 20:29:20 +0000

    Implemented CI pipeline

--- app-cdk/lib/pipeline-cdk-stack.ts
+++ app-cdk/lib/pipeline-cdk-stack.ts
@@ -1,6 +1,9 @@
 import { Stack, StackProps, CfnOutput } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { aws_codeconnections as codeconnections } from 'aws-cdk-lib';
+import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
+import * as codebuild from 'aws-cdk-lib/aws-codebuild';
+import * as codepipeline_actions from 'aws-cdk-lib/aws-codepipeline-actions';
 
 export class PipelineCdkStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps) {
@@ -10,7 +13,52 @@ export class PipelineCdkStack extends Stack {
       connectionName: 'CICD_Workshop_Connection',
       providerType: 'GitHub',
     });
-    
+
+    const pipeline = new codepipeline.Pipeline(this, 'Pipeline', {
+      pipelineName: 'CICD_Pipeline',
+      crossAccountKeys: false,
+      pipelineType: codepipeline.PipelineType.V2,
+      executionMode: codepipeline.ExecutionMode.QUEUED,
+    });
+
+    const codeBuild = new codebuild.PipelineProject(this, 'CodeBuild', {
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.STANDARD_7_0,
+        privileged: true,
+        computeType: codebuild.ComputeType.LARGE,
+      },
+      buildSpec: codebuild.BuildSpec.fromSourceFilename('buildspec_test.yml'),
+    });
+
+    const sourceOutput = new codepipeline.Artifact();
+    const unitTestOutput = new codepipeline.Artifact();
+
+    pipeline.addStage({
+      stageName: 'Source',
+      actions: [
+        new codepipeline_actions.CodeStarConnectionsSourceAction({
+          actionName: 'GitHub',
+          owner: 'steve-krause',
+          repo: 'CICD_Workshop',
+          output: sourceOutput,
+          branch: 'main',
+          connectionArn: 'arn:aws:codeconnections:us-east-2:832356551044:connection/bd9f1156-8bed-49f7-ab20-5fa81d0f346f',
+        }),
+      ],
+    });
+
+    pipeline.addStage({
+      stageName: 'Code-Quality-Testing',
+      actions: [
+        new codepipeline_actions.CodeBuildAction({
+          actionName: 'Unit-Test',
+          project: codeBuild,
+          input: sourceOutput,
+          outputs: [unitTestOutput],
+        }),
+      ],
+    });
+
     new CfnOutput(this, 'SourceConnectionArn', {
       value: SourceConnection.attrConnectionArn,
     });
--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -1,4 +1,5 @@
 /// <reference types='vite/client' />
+/// <reference types='vitest' />
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
@@ -12,4 +13,8 @@ export default defineConfig({
     port: 8081,
     allowedHosts: ["d2otfhqz8weojq.cloudfront.net"]
   },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },  
 })
